8cce97f9d7b0d303bb92442398b58c6c1b97e37d
Diogo Neves <diogo.neves@gmail.com>
2025-11-16 15:05:31 +0000

    add Loopie deps instantiation test

---
+++ backend/tests/services/test_agent_deps.py
@@ -0,0 +1,40 @@
+from app.services.agent import LoopieDeps
+from app.services.agent_tools.repositories import JournalRepository, YouTubeRepository
+
+
+class _EntryService:
+    def list_entries(self):
+        return []
+
+
+class _YoutubeService:
+    async def fetch_channel_feed(self, *args, **kwargs):
+        raise NotImplementedError
+
+    async def fetch_video_detail(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class _UserService:
+    def get_active_user(self):
+        return None
+
+
+class _OAuthService:
+    pass
+
+
+def test_loopie_deps_accepts_repository_instances():
+    deps = LoopieDeps(
+        user_id="user-123",
+        journal_repo=JournalRepository(_EntryService()),
+        youtube_repo=YouTubeRepository(
+            _YoutubeService(),
+            _UserService(),
+            _OAuthService(),
+        ),
+    )
+
+    assert deps.user_id == "user-123"
+    assert isinstance(deps.journal_repo, JournalRepository)
+    assert isinstance(deps.youtube_repo, YouTubeRepository)
